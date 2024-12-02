277fc2fa716eda3ac38c7dc1239b5f37a354e17f
Васильев Дмитрий <Kotee-47@yandex.ru>
2024-12-02 16:40:03 +0300

    добавлена папка, пару веток и второй  main.py

---
+++ 12_02_december_git2/main.py
@@ -0,0 +1,14 @@
+def my_superfunction():
+    print('какои крутои приньт')
+
+
+def main():
+    print('май перви гит програм')
+    print('ия меняю ее кажды день')
+    print('опять')
+    print('хххззщщыв')
+    my_superfunction()
+
+
+if __name__ == '__main__':
+    main()
