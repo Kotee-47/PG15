24cd4e8db21bcb91e158eb8a968a4d41a01962bf
Васильев Дмитрий <Kotee-47@yandex.ru>
2024-12-05 17:01:58 +0300

    добавили функции рисования

--- 12_05_dec_PYGAME1/main2.py
+++ 12_05_dec_PYGAME1/main2.py
@@ -1,9 +1,11 @@
 # Example file showing a basic pygame "game loop"
+import random
+
 import pygame
 
 
-def draw(screeen):
-    screeen.fill((0, 0, 0))
+def draw(screen):
+    screen.fill((0, 0, 0))
     font = pygame.font.Font(None, 50)
     text = font.render('Hello, Pygame!',
                        True,
@@ -12,14 +14,29 @@ def draw(screeen):
     text_h = text.get_height()
     text_x = width // 2 - text_w // 2
     text_y = height // 2 - text_h // 2
-    screeen.blit(text, (text_x, text_y))
-    pygame.draw.rect(screeen, (0, 100, 255),
-                     (text_x - 10,  text_y - 10, text_w + 10,  text_h + 10), 1)
+    screen.blit(text, (text_x, text_y))
+    pygame.draw.rect(screen, (0, 100, 255),
+                     (text_x - 10,  text_y - 10, text_w + 20,  text_h + 20), 1)
+
+
+def draw2(screen):
+    for i in range(1000):
+        screen.fill(pygame.Color('white'),
+                    (random.random() * width,
+                     random.random() * height, 2, 2))
+
+
+def draw3(screen):
+    screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, (0, 255, 0),
+                     (100, 100,
+                      200, 200), 1)
+    pygame.draw.polygon(screen, pygame.Color('orange'), [(0, 0), (150, 50), (50, 150)], 0)
 
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 running = True
 
@@ -33,10 +50,12 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("black")
 
     # RENDER YOUR GAME HERE
-    draw(screen)
+    # draw(screen)
+    # draw2(screen)
+    draw3(screen)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
